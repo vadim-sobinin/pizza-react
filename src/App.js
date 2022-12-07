4e2153e69c6a7eca8fa747806e720d5ba89132a1
vadim-sobinin <vadimsobinin@yandex.ru>
2022-12-07 12:19:43 +0300

    fix adaptive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
@@ -13,6 +14,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
